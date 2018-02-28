2dfa61e8b4748282a46dd944f3b55a4883e01a37
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-28 19:34:58 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {
         :name => "Lord Montague",
-        :age = > "53"
+        :age => "53"
         },
       :matriarch => {
         :name => "Lady Montague",
@@ -14,7 +14,7 @@ def third_challenge
         :age => "15"
         },
       :hero_friends => []
-   },
+   }, 
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet"
@@ -32,5 +32,5 @@ def third_challenge
    }
   }
 
-
+  
 end
